39007ed34dcb79497e802f13da6c2e521cde9089
Jose Rojas <joseyx041199@gmail.com>
2023-08-12 19:20:54 -0400

    added router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import BaseContainer from './components/BaseContainer.vue';
-import BaseButton from './components/BaseButton';
+import { createApp } from "vue";
+// import App from "./App.vue";
+import BaseContainer from "./components/BaseContainer.vue";
+import BaseButton from "./components/BaseButton";
+import router from "./router";
+import HomeViewVue from "./views/HomeView.vue";
 
-const app = createApp(App);
-app.component('base-container', BaseContainer);
-app.component('base-button', BaseButton);
+const app = createApp(HomeViewVue).use(router);
+app.component("base-container", BaseContainer);
+app.component("base-button", BaseButton);
 
-app.mount('#app');
+app.mount("#app");
